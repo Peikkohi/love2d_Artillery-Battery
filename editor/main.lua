28a81d6d703adbf5d43a1449d96f53ca03fe9fe2
Peikko Kalliokoski <peikko@pizbam.verkkotammi>
2019-07-21 20:54:10 +0300

    Added editors camera movement.

--- editor/main.lua
+++ editor/main.lua
@@ -10,9 +10,7 @@ local tiles = {}
 local selected = 0
 
 local camera = {x = 0, y = 0}
-local mouse = {x = love.mouse.getX, y = love.mouse.getY, bt = {}}
-
-local rx, ry
+local mouse = {x = love.mouse.getX, y = love.mouse.getY, bt = love.mouse.isDown}
 
 function love.load()
   local ww, wh = love.window.getDesktopDimensions()
@@ -33,39 +31,42 @@ function love.load()
 end
 
 function love.update(dt)
-  rx = mouse.x() - mouse.x() % BOX_SIZE
-  ry = mouse.y() - mouse.y() % BOX_SIZE
+  local up = love.keyboard.isDown('w') and 1 or 0
+  local dn = love.keyboard.isDown('s') and 1 or 0
+  local rt = love.keyboard.isDown('d') and 1 or 0
+  local lt = love.keyboard.isDown('a') and 1 or 0
 
-  if (mouse.bt[1] and 1 or 0) + (mouse.bt[2] and 1 or 0) == 1 then
-    local cx = rx / BOX_SIZE
-    local cy = ry / BOX_SIZE
+  camera.x = camera.x + (lt - rt) * dt * 100
+  camera.y = camera.y + (up - dn) * dt * 100
 
-    if mouse.bt[1] then
-      map[cx..','..cy] = selected + 1
-    else
-      map[cx..','..cy] = nil
-    end
+  if (mouse.bt(1) and 1 or 0) + (mouse.bt(2) and 1 or 0) == 1 then
+    local rx = mouse.x() - math.floor(camera.x)
+    local ry = mouse.y() - math.floor(camera.y)
+    rx = rx - rx % BOX_SIZE
+    ry = ry - ry % BOX_SIZE
+
+    map[rx ..','.. ry] = mouse.bt(1) and (selected + 1) or nil
   end
 end
 
 function love.draw()
 
-  for k, tile in pairs(map) do
-    local x, y = k:match('(%S+),(%S+)')
-    x, y = x * BOX_SIZE, y * BOX_SIZE
-    love.graphics.draw(tileset, tiles[tile], x, y, 0, SCALE)
+  local cx = math.floor(camera.x)
+  local cy = math.floor(camera.y)
+  local ww, wh = love.graphics.getDimensions()
+
+  for x = cx % BOX_SIZE - BOX_SIZE, ww, BOX_SIZE do
+    for y = cy % BOX_SIZE - BOX_SIZE, wh, BOX_SIZE do
+      local tile = map[x - cx ..','.. y - cy]
+      if tile then love.graphics.draw(tileset, tiles[tile], x, y, 0, SCALE) end
+    end
   end
+
+  local rx = mouse.x() - (mouse.x() - cx) % BOX_SIZE
+  local ry = mouse.y() - (mouse.y() - cy) % BOX_SIZE
 
   love.graphics.draw(tileset, tiles[selected + 1], rx, ry, 0, SCALE)
   love.graphics.rectangle('line', rx, ry, BOX_SIZE, BOX_SIZE)
-end
-
-function love.mousepressed(x, y, button, isTouch)
-  mouse.bt[button] = true
-end
-
-function love.mousereleased(x, y, button, isTouch)
-  mouse.bt[button] = nil
 end
 
 function love.wheelmoved(x, y)
